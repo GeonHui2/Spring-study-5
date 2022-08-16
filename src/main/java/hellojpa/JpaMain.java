defdfa9dd80e273f49e2e1748991554288d77ead
GeonHui Lee <022686@naver.com>
2022-08-16 22:59:34 +0900

    JPA Study

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+        //엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
+
+        EntityManager em = enf.createEntityManager();
+        //엔티티 매니저는 쓰레드간에 공유X (사용하고 버려야 한다).
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//         Member findMember = em.find(Member.class, 1L);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        enf.close();
+    }
+}
