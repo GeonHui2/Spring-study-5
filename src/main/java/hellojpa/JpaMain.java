24772d34d21a3ba231cf65bd5b1d3624c04a0cf6
GeonHui Lee <022686@naver.com>
2022-08-21 21:13:28 +0900

    JPA Study

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,8 +19,16 @@ public class JpaMain {
 
         try {
 
-            Member member = em.find(Member.class, 1L);
-            printMemberAndTeam(member);
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
             tx.commit();
         } catch (Exception e) {
@@ -30,14 +38,5 @@ public class JpaMain {
         }
 
         enf.close();
-    }
-
-    private static void printMemberAndTeam(Member member) {
-
-        String username = member.getUsername();
-        System.out.println("username = " + username);
-
-        Team team = member.getTeam();
-        System.out.println("team = " + team);
     }
 }
